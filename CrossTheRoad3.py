6af9612cf7bb8e7fd64432cb01d307702bc5aebf
achmadasrori99 <93568999+achmadasrori99@users.noreply.github.com>
2021-11-28 22:58:57 +0700

    Add files via upload

---
+++ CrossTheRoad3.py
@@ -0,0 +1,168 @@
+import pygame
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+SCREEN_TITLE = 'STOP&RUN'
+
+clock = pygame.time.Clock()
+
+WHITE_COLOR = (255, 255, 255)
+BALCK_COLOR = (0, 0, 0)
+
+pygame.font.init()
+font = pygame.font.SysFont('ComicSans', 75)
+
+class Game:
+    TICK_RATE = 60
+
+    def __init__(self, image_path, title,width,height ):  
+        self.title = title
+        self.width = width
+        self.height = height
+        self.game_screen = pygame.display.set_mode((width, height))
+        self.game_screen.fill(WHITE_COLOR)
+        pygame.display.set_caption(title)
+        background_image = pygame.image.load(image_path)
+        self.image = pygame.transform.scale(background_image, (width, height))
+
+    def run_game_loop(self, level_speed):
+        is_game_over = False
+        did_win = False
+        direction = 0
+        player_character = PlayerCharacter('asset/pemain.png', 375, 700, 50, 50)
+        enemy_1 = EnemyCharacter('asset/musuh1.png', 20, 600, 50, 50)
+        enemy_1.SPEED *= level_speed
+        enemy_2 = EnemyCharacter('asset/musuh2.png', self.width - 40, 400, 50, 50)
+        enemy_2.SPEED *= level_speed
+        enemy_3 = EnemyCharacter('asset/musuh3.png', 20, 50, 50, 50)
+        enemy_3.SPEED *= level_speed
+        onepiece = GameObject('asset/onepiece.png',375, 50, 50, 50)
+
+        while not is_game_over:
+            for event in pygame.event.get():  
+                if event.type == pygame.QUIT:
+                    is_game_over = True
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        direction = 1
+                    elif event.key == pygame.K_DOWN:
+                        direction = -1
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                        direction = 0
+                print(event)
+
+            self.game_screen.fill(WHITE_COLOR)
+            self.game_screen.blit(self.image, (0,0))
+
+            onepiece.draw(self.game_screen)
+            player_character.move(direction)
+            player_character.draw(self.game_screen)
+            enemy_1.move(self.width)
+            enemy_1.draw(self.game_screen)
+
+            if level_speed > 2:
+                enemy_2.move(self.width)
+                enemy_2.draw(self.game_screen)
+            if level_speed > 4:
+                enemy_3.move(self.width)
+                enemy_3.draw(self.game_screen)
+
+            if player_character.detection_collision(enemy_1):
+                is_game_over = True
+                did_win = False
+                text = font.render('You Lose! :(', True, BALCK_COLOR)
+                self.game_screen.blit(text, (300, 350))
+                pygame.display.update()
+                clock.tick(1)
+                break
+
+            elif player_character.detection_collision(enemy_2):
+                is_game_over = True
+                did_win = False
+                text = font.render('You Lose! :(', True, BALCK_COLOR)
+                self.game_screen.blit(text, (300, 350))
+                pygame.display.update()
+                clock.tick(1)
+                break
+
+            elif player_character.detection_collision(enemy_3):
+                is_game_over = True
+                did_win = False
+                text = font.render('You Lose! :(', True, BALCK_COLOR)
+                self.game_screen.blit(text, (300, 350))
+                pygame.display.update()
+                clock.tick(1)
+                break
+
+            elif player_character.detection_collision(onepiece):
+                is_game_over = True
+                did_win = True
+                text = font.render('You Win! :)', True, BALCK_COLOR)
+                self.game_screen.blit(text, (300, 350))
+                pygame.display.update()
+                clock.tick(1)
+                break
+
+            pygame.display.update()
+            clock.tick(self.TICK_RATE)
+
+        if did_win:
+            self.run_game_loop(level_speed + 0.5)
+        else:
+            return
+
+class GameObject:
+    def __init__(self, image_path, x, y, width, height):
+        self.x_pos = x
+        self.y_pos = y
+        self.width = width
+        self.height = height
+        object_image = pygame.image.load(image_path)
+        self.image = pygame.transform.scale(object_image,(width, height))
+
+    def draw(self, background):
+        background.blit(self.image, (self.x_pos,self.y_pos))
+
+class PlayerCharacter(GameObject):
+    SPEED = 10
+
+    def __init__(self, image_path, x, y, width, height):
+        super().__init__(image_path, x, y, width, height)
+
+    def move(self, direction):
+        if direction > 0:
+            self.y_pos -= self.SPEED
+        elif direction < 0:
+            self.y_pos += self.SPEED
+
+    def detection_collision(self, other_body):
+        if self.y_pos > other_body.y_pos + other_body.height:
+            return False
+        elif self.y_pos + self.height < other_body.y_pos:
+            return False
+        if self.x_pos > other_body.x_pos + other_body.width:
+            return False
+        elif self.x_pos + self.width < other_body.x_pos:
+            return False
+        return True
+
+class EnemyCharacter(GameObject):
+    SPEED = 10
+
+    def __init__(self, image_path, x, y, width, height):
+        super().__init__(image_path, x, y, width, height)
+
+    def move(self, max_width):
+        if self.x_pos <= 20:
+            self.SPEED = abs(self.SPEED)
+        elif self.x_pos >= max_width - 48:
+            self.SPEED = -abs(self.SPEED)
+        self.x_pos += self.SPEED
+        
+pygame.init()
+new_game = Game('asset/background.png', SCREEN_TITLE, SCREEN_WIDTH, SCREEN_HEIGHT)
+new_game.run_game_loop(1)
+
+pygame.quit()
+quit()
